503c109672d5512d2e6b11859e9eb6cf1133fc3b
tripzzy <mjmananghaya@gmail.com>
2023-09-01 18:59:22 +0800

    Added SecurityConfig class.

---
+++ src/main/java/com/horizon/core/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.horizon.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/auth/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(sessionConfig -> {
+                    sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+        return http.build();
+    }
+}
